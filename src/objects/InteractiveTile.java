2cc060fd7a53b891395ea073874623d13ff74211
lmweav <30388630+lmweav@users.noreply.github.com>
2018-04-26 14:37:06 +0100

    Add files via upload

--- src/objects/InteractiveTile.java
+++ src/objects/InteractiveTile.java
@@ -28,8 +28,6 @@ public class InteractiveTile extends Tile {
 
     public boolean isMenu() { return menu; }
 
-    public void changeMenu() { menu = !menu; }
-
     /**
      * Initialises the co-ordinates of a map's interactive points.
      *
